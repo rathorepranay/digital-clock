624548465bf29670a3e39b0f953db9f1cff6717a
rathorepranay <rathorepranay61@gmail.com>
2025-10-21 19:08:40 +0530

    Fixed Commit

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 const hourField = document.getElementsByClassName("hour")[0];
 const minField = document.querySelector(".minutes");
 const secField = document.querySelector(".seconds");
-const cont = document.querySelector(".container")
+const cont = document.querySelector(".container");
+
+let hourNum, minutes, seconds;
+
 function getValues() {
   const now = new Date();
   hourNum = now.getHours();
   minutes = now.getMinutes();
   seconds = now.getSeconds();
-  hour = hour<10 ? "0" + hour : hour
-  minutes = minutes<10 ? "0" + minutes : minutes
-  seconds = seconds<10 ? "0" + seconds : seconds
-}
-function changeBG(){
-    if(hourNum >= 6 && hourNum<18){
-        cont.style.backgroundColor = "white"
-        cont.style.color = "black"
-    }
-    else{
-        cont.style.backgroundColor = "black"
-        cont.style.color = "white"
-    }
-}
-function editValues() {
-  hourField.innerHTML = hour;
-  minField.innerHTML = minutes;
-  secField.innerHTML = seconds;
+
+  // Fix: Use hourNum instead of undefined hour variable
+  const hour = hourNum < 10 ? "0" + hourNum : hourNum;
+  const min = minutes < 10 ? "0" + minutes : minutes;
+  const sec = seconds < 10 ? "0" + seconds : seconds;
+
+  return { hour, min, sec };
 }
 
-getValues();
+function changeBG() {
+  if (hourNum >= 6 && hourNum < 18) {
+    cont.style.backgroundColor = "white";
+    cont.style.color = "black";
+  } else {
+    cont.style.backgroundColor = "black";
+    cont.style.color = "white";
+  }
+}
+
+function editValues() {
+  const { hour, min, sec } = getValues();
+  hourField.innerHTML = hour;
+  minField.innerHTML = min;
+  secField.innerHTML = sec;
+}
+
 editValues();
 changeBG();
 setInterval(() => {
-  getValues();
   editValues();
   changeBG();
 }, 1000);
